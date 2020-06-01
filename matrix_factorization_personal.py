b476e7062788d2f9e9abce6aef746ac13f029643
kchung1995 <45638155+kchung1995@users.noreply.github.com>
2020-06-01 16:21:34 +0900

    week 12 commition

--- matrix_factorization_personal.py
+++ matrix_factorization_personal.py
@@ -8,12 +8,12 @@ import numpy as np
 import warnings
 warnings.filterwarnings("ignore")
 
-ratings = pd.read_csv('./movie_dataset/ratings.csv')
-foods = pd.read_csv('./movie_dataset/movies.csv')
+ratings = pd.read_csv('./food_dataset/ratings_partial.csv')
+foods = pd.read_csv('./food_dataset/foods.csv')
 
 user_food_ratings = ratings.pivot(
     index = 'userId',
-    columns = 'movieId',
+    columns = 'foodId',
     values = 'rating'
 ).fillna(0)
 
@@ -45,14 +45,14 @@ def recommend_foods(svd_preds, user_id, ori_foods, ori_ratings, num_recommendati
     user_row_number = user_id-1
     sorted_user_predictions = svd_preds.iloc[user_row_number].sort_values(ascending=False)
     user_data = ori_ratings[ori_ratings.userId == user_id]
-    user_history = user_data.merge(ori_foods, on = 'movieId').sort_values(['rating'], ascending = False)
-    recommendations = ori_foods[~ori_foods['movieId'].isin(user_history['movieId'])]
-    recommendations = recommendations.merge(pd.DataFrame(sorted_user_predictions).reset_index(), on = 'movieId')
+    user_history = user_data.merge(ori_foods, on = 'foodId').sort_values(['rating'], ascending = False)
+    recommendations = ori_foods[~ori_foods['foodId'].isin(user_history['foodId'])]
+    recommendations = recommendations.merge(pd.DataFrame(sorted_user_predictions).reset_index(), on = 'foodId')
     recommendations = recommendations.rename(columns = {user_row_number: 'Predictions'}).sort_values('Predictions', ascending = False).iloc[:num_recommendations, :]
 
     return user_history, recommendations
 
-already_rated, predictions = recommend_foods(svd_preds, 178, foods, ratings, 10)
+already_rated, predictions = recommend_foods(svd_preds, 7, foods, ratings, 10)
 
 pd.set_option('display.max_columns', None)
 print(already_rated.head(10))
